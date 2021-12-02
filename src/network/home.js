1fc7e7f4adb1119b291dc4e26c7eebf624a6c962
aq949342769 <949342769@qq.com>
2021-12-02 20:37:31 +0800

    axios二次封装说明

--- src/network/home.js
+++ src/network/home.js
@@ -5,12 +5,13 @@ import {
 
 export function getNews(type, page) {
   return request({
-    method: 'get',//or post.....
+    method: 'get', //or post.....
     url: '/home/data',
     params: {
       type,
       page
     }
     //or other options
+    //for more info check in http://axios-js.com/zh-cn/docs/index.html#%E8%AF%B7%E6%B1%82%E9%85%8D%E7%BD%AE
   })
 }
